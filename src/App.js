e81a222a3e5550011405ce19d59ef49fb686635b
Hyeyun <yuuuuuuni@gmail.com>
2021-05-23 22:36:53 +0900

    react map test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
-function PropsTest(props) {
-  console.log(props);
-  // {aaa} = props;
+const mapTest  = [
+  { name: 'map1', index: 1 },
+  { name: 'map2', index: 2 },
+  { name: 'map3', index: 3 },
+  { name: 'map4', index: 4 },
+  { name: 'map5', index: 5 },
+  { name: 'map6', index: 6 },
+  { name: 'map7', index: 7 },
+  { name: 'map8', index: 8 },
+  { name: 'map9', index: 9 },
+]
+
+function PropsTest({name, index}) {
   return (
     <div>
-      <h3>{props.test}</h3>
-      <h3></h3>
+      <h3>{name}</h3>
+      <h3>{index}</h3>
     </div>
   );
 }
 function App() {
   return (
     <div>
-      <h1>test</h1>
-      <PropsTest test="xxxxx" array={[1, 2, 3, 4, 5]} bool={true}/>
+      {mapTest.map((test) => (<PropsTest name={test.name} index={test.index}/>))}
     </div>
   );
 }
