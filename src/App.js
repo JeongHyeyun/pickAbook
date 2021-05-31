50ab93e57a17c99d247e821eb66490f040e1f54a
Hyeyun <yuuuuuuni@gmail.com>
2021-05-31 21:28:49 +0900

    init loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
-const mapTest  = [
-  { name: 'map1', index: 1 },
-  { name: 'map2', index: 2 },
-  { name: 'map3', index: 3 },
-  { name: 'map4', index: 4 },
-  { name: 'map5', index: 5 },
-  { name: 'map6', index: 6 },
-  { name: 'map7', index: 7 },
-  { name: 'map8', index: 8 },
-  { name: 'map9', index: 9 },
-]
-
-function PropsTest({name, index}) {
-  return (
-    <div>
-      <h3>{name}</h3>
-      <h3>{index}</h3>
-    </div>
-  );
-}
-function App() {
-  return (
-    <div>
-      {mapTest.map((test) => (<PropsTest name={test.name} index={test.index}/>))}
-    </div>
-  );
+class App extends React.Component {
+  state = {  // 사용할 변수 선언
+    isLoading: true,  // 로딩여부
+    movies: [],  // 보여줄 영화 정보를 담는 배열
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false, moveis: []});  // state값 변경
+    }, 5000); // 5초 뒤
+  }
+  render() {
+    const { isLoading } = this.state; // state에서 isLoading 값 추출
+    return <div>{ isLoading ? '로딩중...!' : '로딩 끝' }</div>;  // isLoading === true ? '로딩중'
+  }
 }
 
 export default App;
